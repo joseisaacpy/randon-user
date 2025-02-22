7278c1f031b041e8a306ba798fab70500c931ffd
José Isaac <joseisaacnascimento@gmail.com>
2025-02-22 14:59:32 -0300

    Adiciona o try e o catch no js.

--- index.js
+++ index.js
@@ -2,14 +2,20 @@
 const URL = "https://randomuser.me/api/";
 // função de pegar dados
 async function pegarDados() {
-  // faz requisicao com fetch
-  let response = await fetch(URL);
-  // transforma response num json
-  let data = await response.json();
-  // resultado
-  let result = data.results[0];
-  // mostra response no console (teste)
-  console.log(result);
+  // try
+  try {
+    // faz requisicao com fetch
+    let response = await fetch(URL);
+    // transforma response num json
+    let data = await response.json();
+    // resultado
+    let result = data.results[0];
+    // mostra response no console (teste)
+    console.log(result);
+  } catch (error) {
+    // se der erro
+    console.error(error);
+  }
 }
 // chama a funcao
 pegarDados();
