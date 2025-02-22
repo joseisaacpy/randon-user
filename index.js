e453c9c7c8b6e0ba648d9f2efb9fbaa4b6fba04d
José Isaac <joseisaacnascimento@gmail.com>
2025-02-22 14:58:04 -0300

    Criação da div card, para exibir o usuário.

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ async function pegarDados() {
   let response = await fetch(URL);
   // transforma response num json
   let data = await response.json();
+  // resultado
+  let result = data.results[0];
   // mostra response no console (teste)
-  console.log(data);
+  console.log(result);
 }
 // chama a funcao
-pegarDados()
+pegarDados();
